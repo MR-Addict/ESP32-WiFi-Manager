46aa5a3ce1171558e7864651c429b0f663c1f194
MR-Addcit <2750417853@qq.com>
2022-03-26 12:08:19 +0800

    Improve OLED UI

--- WiFi_Manager/tasks.h
+++ WiFi_Manager/tasks.h
@@ -31,29 +31,32 @@ void task4callback() {
         String formattedTime = timeClient.getFormattedTime();
 
         oled.clear();
-        oled.setFont(&Orbitron_Bold_16);
         // Show weather status
         if (weather.weather == "Rain" || weather.weather == "Drizzle") {
-            oled.drawBitmap(80, 0, 40, 40, rain_40x40);
+            oled.drawBitmap(80, 25, 40, 40, rain_40x40);
         } else if (weather.weather == "Thunderstorm") {
-            oled.drawBitmap(80, 0, 40, 40, thunderstorm_40x40);
+            oled.drawBitmap(80, 25, 40, 40, thunderstorm_40x40);
         } else if (weather.weather == "Snow") {
-            oled.drawBitmap(80, 0, 40, 40, snow_40x40);
+            oled.drawBitmap(80, 25, 40, 40, snow_40x40);
         } else if (weather.weather == "Clouds") {
-            oled.drawBitmap(80, 5, 40, 32, cloud_40x32);
+            oled.drawBitmap(80, 30, 40, 32, cloud_40x32);
         } else if (weather.weather == "Clear") {
-            oled.drawBitmap(80, 5, 40, 32, sun_40x32);
+            oled.drawBitmap(80, 30, 40, 32, sun_40x32);
         } else {
-            oled.drawBitmap(80, 5, 40, 32, mist_40x32);
+            oled.drawBitmap(80, 30, 40, 32, mist_40x32);
         }
+        oled.drawBitmap(112, 4, 16, 16, signal_16x16);
         // Show temp and humidity
+        oled.setFont(&Orbitron_Bold_16);
         char message[50] = {0};
         sprintf(message, "%3.1f-C", weather.temp);
-        oled.print(0, 0, message);
+        oled.print(0, 24, message);
         sprintf(message, "%3.1f%c", weather.humidity, '%');
-        oled.print(0, 20, message);
+        oled.print(0, 44, message);
         // Show time
-        oled.print(0, 40, formattedTime.c_str());
+        oled.setFont(&Dialog_bold_10);
+        oled.drawFastHLine(0, 20, 128);
+        oled.print(30, 4, formattedTime.c_str());
         oled.show();
     }
 }
